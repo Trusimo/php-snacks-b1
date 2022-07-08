fed2550cd5936724d6e96578c5d1648d4446f992
Trusimo <simonetrunfio96@gmail.com>
2022-07-08 17:17:43 +0200

    snack 4 completed

--- Snack4.php
+++ Snack4.php
@@ -1,3 +1,12 @@
 <!-- Creare un array con 15 numeri casuali,
 tenendo conto che l’array non dovrà contenere lo stesso numero
 più di una volta -->
+<?php
+for($i = 0; $i <= 14; $i++){
+    $array[] = mt_rand(0, 100);
+}
+
+
+?>
+
+<h1> <?php var_dump($array) ?>  </h1>
