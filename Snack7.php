3d2772a1f408b9ffb8ff8e7f7cf49435e159f0a2
Trusimo <simonetrunfio96@gmail.com>
2022-07-08 17:29:54 +0200

    un voto unico(?)

--- Snack7.php
+++ Snack7.php
@@ -1,3 +1,60 @@
 <!-- Creare un array contenente qualche alunno di un’ipotetica classe.
 Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici.
 Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+
+<?php
+    $classe = [
+        [
+            "nome" => "Jerry",
+            "cognome" => "Calà",
+            [
+                "matematica" => 5,
+                "italiano" => 8,
+                "storia" => 7,
+                "fisica" => 4,
+                "filosofia" => 7
+            ]
+        ],
+        [
+            "nome" => "Massimo",
+            "cognome" => "Boldi",
+            $voti = [
+                "matematica" => 10,
+                "italiano" => 4,
+                "storia" => 5,
+                "fisica" => 8,
+                "filosofia" => 6
+            ]
+        ],
+        [
+            "nome" => "Christian",
+            "cognome" => "De Sica",
+            $voti = [
+                "matematica" => 6,
+                "italiano" => 9,
+                "storia" => 4,
+                "fisica" => 8,
+                "filosofia" => 7
+            ]
+        ],
+        [
+            "nome" => "Ezio",
+            "cognome" => "Greggio",
+            $voti = [
+                "matematica" => 8,
+                "italiano" => 8,
+                "storia" => 5,
+                "fisica" => 9,
+                "filosofia" => 4
+            ]
+        ],
+    ]
+?>
+
+<?php
+    for ($i = 0; $i < count($classe); $i++) {
+        $alunno = $classe[$i];
+    ?>
+
+        <div><?php echo $alunno["nome"] . " " . $alunno["cognome"] . " - " . array_sum($voti) / count($voti)?></div>
+<?php } ?>
